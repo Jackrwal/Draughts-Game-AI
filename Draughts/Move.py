9fdaf44f36e96eea3d7e76c0f68fe08661611944
Jackrwal <jackrwal@gmail.com>
2020-04-17 17:43:53 +0100

    updated Move object to support sequences of moves, for capture sequences

--- Draughts/Move.py
+++ Draughts/Move.py
@@ -4,6 +4,11 @@ class Move:
     __player = ""
     __piece = object
     __target = ""
+    __score = -1
+
+    # used for tracking sequences of moves
+    __next = object
+    __prev = object
 
     # constructs a Move object
     #   player - The Player object representing the player that makes the move
@@ -14,6 +19,11 @@ class Move:
         self.__piece = piece
         self.__target = target
 
+    # adds a new move to the sequence of moves, after this object
+    def addToSequence(self, move):
+        __next = move
+        move.setPrev(self)
+
     def getPlayer(self):
         return self.__player
 
@@ -22,6 +32,21 @@ class Move:
 
     def getTarget(self):
         return self.__target
+
+    def getScore(self):
+        return self.__score
+
+    def setScore(self, score):
+        self.__score = score
+
+    def getNext(self):
+        return self.__next
+
+    def getPrev(self):
+        return self.__prev
+
+    def setPrev(self, move):
+        self.__prev = move
 
 
 
